c2ace3feff999d6e10497c801593a77c759fb28b
Jan Wirth <elarkaso@gmail.com>
2026-06-02 20:31:05 +0200

    rnm 2

--- apps/frontend/src/pages/ExerciseOverViewPage.jsx
+++ apps/frontend/src/pages/ExerciseOverViewPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { getExercises, deleteExercise } from "../api/exerciseApi";
-
 import { MUSCLE_GROUPS } from "../utils/muscleGroups";
 
 import { ErrorMessage } from "../components/messages/ErrorMessage";
